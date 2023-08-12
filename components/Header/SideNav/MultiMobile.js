b87a48314eac1e53ae4819a6c8f899251d6b4564
Azhar Ali Mohammad <md.azhar.ali@knights.ucf.edu>
2023-08-12 13:12:59 -0400

    fixed mobile navigation

--- components/Header/SideNav/MultiMobile.js
+++ components/Header/SideNav/MultiMobile.js
@@ -11,6 +11,7 @@ import ArrowDropUp from '@mui/icons-material/ArrowDropUp';
 import ArrowDropDown from '@mui/icons-material/ArrowDropDown';
 import Collapse from '@mui/material/Collapse';
 import { useTranslation } from 'next-i18next';
+import { ListItemButton } from '@mui/material';
 import link from '~/public/text/link';
 import useStyles from '../sidenav-style';
 import navMenu from '../data/multiple';
@@ -28,7 +29,7 @@ function MobileMenu(props) {
   const handleToggle = (id) => {
     setExpand({
       ...expand,
-      [id]: !expand[id]
+      [id]: !expand[id],
     });
   };
 
@@ -45,32 +46,41 @@ function MobileMenu(props) {
           if (subitem.child) {
             return (
               <div key={index.toString()}>
-                <ListItem
-                  button
-                  className={cx(menu[subitem.id] ? classes.current : '', classes.hasGrandChild)}
+                <ListItemButton
+                  className={cx(
+                    menu[subitem.id] ? classes.current : '',
+                    classes.hasGrandChild,
+                  )}
                   onClick={() => handleToggle(subitem.id)}
                 >
                   {menu[subitem.id] ? <ArrowDropUp /> : <ArrowDropDown />}
-                  <ListItemText className={classes.menuList} primary={subitem.name} />
-                </ListItem>
-                { childMenu(expand, subitem) }
+                  <ListItemText
+                    className={classes.menuList}
+                    primary={subitem.name}
+                  />
+                </ListItemButton>
+                {childMenu(expand, subitem)}
               </div>
             );
           }
           return (
-            <ListItem
+            <ListItemButton
               key={index.toString()}
               className={cx(
                 classes.noChild,
                 classes.sideGroupLink,
-                curURL === curOrigin + langPath + subitem.link ? classes.current : ''
+                curURL === curOrigin + langPath + subitem.link
+                  ? classes.current
+                  : '',
               )}
               component="a"
               href={subitem.link}
-              button
             >
-              <ListItemText className={classes.menuList} primary={subitem.name} />
-            </ListItem>
+              <ListItemText
+                className={classes.menuList}
+                primary={subitem.name}
+              />
+            </ListItemButton>
           );
         })}
       </List>
@@ -83,13 +93,10 @@ function MobileMenu(props) {
       onClose={toggleDrawer}
       onOpen={toggleDrawer}
       classes={{
-        paper: classes.paperNav
+        paper: classes.paperNav,
       }}
     >
-      <div
-        className={classes.mobileNav}
-        role="presentation"
-      >
+      <div className={classes.mobileNav} role="presentation">
         <div className={open ? classes.menuOpen : ''}>
           <List
             component="nav"
@@ -100,27 +107,36 @@ function MobileMenu(props) {
               if (item.child) {
                 return (
                   <div key={index.toString()}>
-                    <ListItem
-                      button
+                    <ListItemButton
                       className={expand[item.id] ? classes.currentParent : ''}
                       onClick={() => handleToggle(item.id)}
                     >
-                      <ListItemText className={classes.menuList} primary={item.name} />
+                      <ListItemText
+                        className={classes.menuList}
+                        primary={item.name}
+                      />
                       {expand[item.id] ? <ExpandLess /> : <ExpandMore />}
-                    </ListItem>
-                    { childMenu(expand, item) }
+                    </ListItemButton>
+                    {childMenu(expand, item)}
                   </div>
                 );
               }
               return (
-                <ListItem
+                <ListItemButton
                   key={index.toString()}
-                  className={cx(classes.noChild, curURL === curOrigin + langPath + item.link ? classes.current : '')}
-                  button
+                  className={cx(
+                    classes.noChild,
+                    curURL === curOrigin + langPath + item.link
+                      ? classes.current
+                      : '',
+                  )}
                   href={item.link}
                 >
-                  <ListItemText className={classes.menuList} primary={item.name} />
-                </ListItem>
+                  <ListItemText
+                    className={classes.menuList}
+                    primary={item.name}
+                  />
+                </ListItemButton>
               );
             })}
           </List>
@@ -129,12 +145,20 @@ function MobileMenu(props) {
             {['login', 'register'].map((text, index) => (
               <ListItem
                 key={index.toString()}
-                className={cx(classes.noChild, curURL === curOrigin + langPath + '/' + text ? classes.current : '')}
+                className={cx(
+                  classes.noChild,
+                  curURL === curOrigin + langPath + '/' + text
+                    ? classes.current
+                    : '',
+                )}
                 component="a"
                 href={link.retail[text]}
                 button
               >
-                <ListItemText className={classes.menuList} primary={t('' + text)} />
+                <ListItemText
+                  className={classes.menuList}
+                  primary={t('' + text)}
+                />
               </ListItem>
             ))}
           </List>
