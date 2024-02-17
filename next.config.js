9fc8e54a3017f3b3ed956afb92876304e7d889de
Azhar Ali Mohammad <md.azhar.ali@knights.ucf.edu>
2024-02-17 17:21:52 -0500

    Fix issue with build

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ module.exports = withImages({
   trailingSlash: true,
   images: {
     disableStaticImages: true,
+    unoptimized: true,
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
